6c46e201c51032ea823e79ab146f5fd25f86dc2c
PentaTTT <duytan0220@gmail.com>
2024-03-17 01:02:56 +0700

    update validate form contact

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Contact = () => {
     const inputCss = 'lg:w-[30%] w-5/6 p-3 rounded-md'
 
     const [error, setError] = useState({})
+    const scriptUrl = "https://script.google.com/macros/s/AKfycby544lCjtb9ZIJux2Kq_qc7VhrcpwR2b1vUoS5TTI13DyC4JW30FwwFYrv5nkKmCHVcIA/exec"
     const [formData, setFormData] = useState({
         "Họ tên": '',
         "SĐT": '',
         "Email": '',
         "Lời nhắn": ''
     })
+    const formRef = useRef(null)
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -29,19 +31,26 @@ const Contact = () => {
         }
         if (!formData['Email'].trim()) {
             validationErrors['Email'] = 'Hãy nhập email!'
-        } else if (!/\S+@\S\.\S+/.test(formData['Lời nhắn'])) {
-            validationErrors['Lời nhắn'] = 'Email không hợp lệ!'
+        }
+        else if (!/[a-zA-Z0-9.-_%+]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g.test(formData['Email'])) {
+            validationErrors['Email'] = 'Email không hợp lệ!'
         }
 
         setError(validationErrors)
         if (Object.keys(validationErrors).length === 0) {
-            alert("Gửi form thành công!")
-            setFormData({
-                "Họ tên": '',
-                "SĐT": '',
-                "Email": '',
-                "Lời nhắn": ''
+            fetch(scriptUrl, {
+                method: 'POST',
+                body: new FormData(formRef.current),
+            }).then(() => {
+                alert("Gửi form thành công!")
+                setFormData({
+                    "Họ tên": '',
+                    "SĐT": '',
+                    "Email": '',
+                    "Lời nhắn": ''
+                })
             })
+
         }
     }
     return (
@@ -53,30 +62,38 @@ const Contact = () => {
             </div>
 
             <div className='pt-7 mx-auto text-center'>
-                <form className="flex flex-col gap-7" method='POST'
-                    //onSubmit={handleSubmit}
-                    action="https://script.google.com/macros/s/AKfycby544lCjtb9ZIJux2Kq_qc7VhrcpwR2b1vUoS5TTI13DyC4JW30FwwFYrv5nkKmCHVcIA/exec"
+                <form className="flex flex-col gap-7"
+                    ref={formRef}
+                    onSubmit={handleSubmit}
+
                 >
                     <div>
                         <label htmlFor=""></label>
                         <input className={inputCss} name='Họ tên' type="text" placeholder='Họ và tên (*)'
+                            value={formData['Họ tên']}
                             onChange={handleChange}
                         />
                         {error['Họ tên'] && <div className='text-red-600'>{error['Họ tên']}</div>}
                     </div>
                     <div>
                         <label htmlFor=""></label>
-                        <input className={inputCss} name='SĐT' type="text" placeholder='Số điện thoại (*)' onChange={handleChange} />
+                        <input className={inputCss} name='SĐT' type="text" placeholder='Số điện thoại (*)'
+                            value={formData['SĐT']}
+                            onChange={handleChange} />
                         {error['SĐT'] && <div className='text-red-600'>{error['SĐT']}</div>}
                     </div>
                     <div>
                         <label htmlFor=""></label>
-                        <input className={inputCss} name='Email' type="text" placeholder='Email của bạn (*)' onChange={handleChange} />
+                        <input className={inputCss} name='Email' type="text" placeholder='Email của bạn (*)'
+                            value={formData['Email']}
+                            onChange={handleChange} />
                         {error['Email'] && <div className='text-red-600'>{error['Email']}</div>}
                     </div>
                     <div>
                         <textarea className={inputCss}
-                            name="Lời nhắn" id='' cols="30" rows="7" placeholder='Lời nhắn' onChange={handleChange}>
+                            name="Lời nhắn" id='' cols="30" rows="7" placeholder='Lời nhắn'
+                            value={formData['Lời nhắn']}
+                            onChange={handleChange}>
                         </textarea>
                     </div>
                     <button className='bg-green btnEffect mx-auto p-3 rounded-md flex items-center text-yellow-400 hover:text-yellow-400' type='submit'
