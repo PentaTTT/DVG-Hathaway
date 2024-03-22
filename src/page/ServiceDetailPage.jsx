44b73fbc083c18fa7411c49a17602a87ea99e84c
PentaTTT <duytan0220@gmail.com>
2024-03-22 17:17:45 +0700

    update data net service

--- src/page/ServiceDetailPage.jsx
+++ src/page/ServiceDetailPage.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
-import { serviceNetFanPage } from '../ultils/serviceDetail'
+import { serviceNetFanPage, serviceNetFanPageExtra } from '../ultils/serviceDetail'
 import videoBg from "/image/video_service.mp4"
 import { Contact, Team } from '../component'
+import MUIDataTable from "mui-datatables";
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 
 const ServiceDetailPage = () => {
+
+    const title = 'Báo Giá'
+
+    const columnsCustome = serviceNetFanPageExtra?.columns?.map(item => ({
+        ['name']: item, ['options']:
+        {
+            customBodyRender: (value) => (
+                <a
+                    href={value}
+                    target='_blank'
+                    className={!value?.indexOf('https://') && 'px-3 py-2 text-white bg-cyan-600 rounded-md'}
+                >{!value?.indexOf('https://') ? 'Xem Fanpage' : value}</a>
+            )
+        }
+    }))
+    const columns = columnsCustome
+
+
+    const data = serviceNetFanPageExtra?.data
+
+    const options = {
+        selectableRows: 'none',
+        rowsPerPage: 10,
+        rowsPerPageOptions: [10, 20, 30, 100],
+        responsive: 'standard'
+    };
+
+    //style table
+    const theme = createTheme({
+        typography: {
+            fontFamily: "Roboto Slab",
+        },
+        palette: {
+            background: {
+                paper: "#fff",
+                default: "#fff",
+            },
+        },
+        components: {
+            MuiTableCell: {
+                styleOverrides: {
+                    head: {
+                        padding: "10px 4px",
+                    },
+                    body: {
+                        padding: "15px 20px",
+                    },
+                }
+            }
+        }
+    })
 
     return (
         <div className=''>
@@ -26,7 +79,7 @@ const ServiceDetailPage = () => {
                         serviceNetFanPage.map((item) => {
                             return (
                                 <div key={item.id}
-                                    className='bg-white flex justify-center p-5 rounded-md shadow-md'
+                                    className='bg-white flex justify-center p-5 rounded-md shadow-md hover:shadow-2xl'
                                 >
                                     <a href={item.link} target='_blank'>
                                         <div>
@@ -38,7 +91,7 @@ const ServiceDetailPage = () => {
                                         <div className='text-center'>
                                             <p className='text-xl font-bold py-5 font-fontRoboto'>{item.name}</p>
                                             <p>Follow: {item.follow}</p>
-                                            <p>Cost: {item.cost}</p>
+                                            <p>Chi Phí Post: {item.cost}</p>
                                         </div>
 
                                     </a>
@@ -48,6 +101,16 @@ const ServiceDetailPage = () => {
                     }
                 </div>
 
+                <div className='py-12 px-5'>
+                    <ThemeProvider theme={theme}>
+                        <MUIDataTable
+                            title={title}
+                            data={data}
+                            columns={columns}
+                            options={options}
+                        />
+                    </ThemeProvider>
+                </div>
             </div>
 
             <Team />
--- src/ultils/partner.js
+++ src/ultils/partner.js
@@ -6,15 +6,19 @@ import zaloImg from '../assets/partner/zalopay.png'
 import vngImg from '../assets/partner/vng.png'
 import travelokaImg from '../assets/partner/traveloka.png'
 import shopeeImg from '../assets/partner/shopee.png'
-import doubleuImg from '../assets/partner/doubleu.jpg'
-import metubImg from '../assets/partner/metub.png'
-import gemImg from '../assets/partner/gem.png'
-import hiipImg from '../assets/partner/hiip.png'
-import chinImg from '../assets/partner/chinmedia.png'
-import comesImg from '../assets/partner/comesmedia.png'
-import theanh28Img from '../assets/partner/theanh28.jpg'
-import umediaImg from '../assets/partner/umedia.png'
-import sefaImg from '../assets/partner/sefamedia.png'
+import baeminImg from '../assets/partner/baemin.png'
+import medlatecImg from '../assets/partner/medlatec.webp'
+import ecoparkImg from '../assets/partner/ecopark.png'
+import facebookImg from '../assets/partner/facebook.png'
+// import doubleuImg from '../assets/partner/doubleu.jpg'
+// import metubImg from '../assets/partner/metub.png'
+// import gemImg from '../assets/partner/gem.png'
+// import hiipImg from '../assets/partner/hiip.png'
+// import chinImg from '../assets/partner/chinmedia.png'
+// import comesImg from '../assets/partner/comesmedia.png'
+// import theanh28Img from '../assets/partner/theanh28.jpg'
+// import umediaImg from '../assets/partner/umedia.png'
+// import sefaImg from '../assets/partner/sefamedia.png'
 
 
 export const listPartner = [
@@ -51,40 +55,56 @@ export const listPartner = [
         img: shopeeImg
     },
     {
-        name: 'Double U',
-        img: doubleuImg
+        name: 'Baemin',
+        img: baeminImg
     },
     {
-        name: 'Metub',
-        img: metubImg
+        name: 'Medlatec',
+        img: medlatecImg
     },
     {
-        name: 'Gem Digital',
-        img: gemImg
+        name: 'Ecopark',
+        img: ecoparkImg
     },
     {
-        name: 'Hiip',
-        img: hiipImg
+        name: 'Facebook',
+        img: facebookImg
     },
-    {
-        name: 'Chin Media',
-        img: chinImg
-    },
-    {
-        name: 'Comes Media',
-        img: comesImg
-    },
-    {
-        name: 'Theanh 28',
-        img: theanh28Img
-    },
-    {
-        name: 'Umedia',
-        img: umediaImg
-    },
-    {
-        name: 'SEFA Media',
-        img: sefaImg
-    },
+    // {
+    //     name: 'Double U',
+    //     img: doubleuImg
+    // },
+    // {
+    //     name: 'Metub',
+    //     img: metubImg
+    // },
+    // {
+    //     name: 'Gem Digital',
+    //     img: gemImg
+    // },
+    // {
+    //     name: 'Hiip',
+    //     img: hiipImg
+    // },
+    // {
+    //     name: 'Chin Media',
+    //     img: chinImg
+    // },
+    // {
+    //     name: 'Comes Media',
+    //     img: comesImg
+    // },
+    // {
+    //     name: 'Theanh 28',
+    //     img: theanh28Img
+    // },
+    // {
+    //     name: 'Umedia',
+    //     img: umediaImg
+    // },
+    // {
+    //     name: 'SEFA Media',
+    //     img: sefaImg
+    // },
 
 ] 
